808d2ed1840878c1cad6fa34c361c339e9448982
Mayur Panchal <mayur.panchal@shoptimize.in>
2022-02-13 10:25:37 +0530

    refactor: :recycle: Update import style of index page
    Add _app.js and import global styles under that by moving <style> in to .scss file

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+// To use global styles create _app.js file in pages folder and import global styles:
+// _app.js is the starting point for your application. It is the place to import things you want to use in your entire project.
+import './global.scss';
+import Home from './index';
+
+export default Home;
